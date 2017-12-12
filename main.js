8bec8a9ac14ebb32e80a1f9c63ecf62bbc437eb3
tquyen1997 <t.quyen1997@yahoo.com>
2017-12-11 21:31:19 -0500

    Team note added

--- main.js
+++ main.js
@@ -4,16 +4,24 @@ function CalcPriceToyota()
 		var a = parseFloat(document.getElementById("day").value);
 		var price = 16.05 * a;
 		document.getElementById("totalprice").value = parseFloat(price).toFixed(2) + "$";
+	}
+
 	
 function CalcPriceHonda()
 	{
 		var a = parseFloat(document.getElementById("day").value);
-		var price = 15.00 * a;
+		var price = 15.05 * a;
 		document.getElementById("totalprice").value = parseFloat(price).toFixed(2) + "$";
-	}	
+	}
+
+
 function CalcPriceMercedes()
 	{
 		var a = parseFloat(document.getElementById("day").value);
-		var price = 20.00 * a;
+		var price = 42.05 * a;
 		document.getElementById("totalprice").value = parseFloat(price).toFixed(2) + "$";
-	}	
+	}
+
+	
+
+
